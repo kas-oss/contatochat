65827c872a6f8a0e2afcd8bbd794d0580b23377c
Lucas Oliveira <lucas-seo@hotmail.com.br>
2022-11-27 22:16:40 -0300

    Adicionando backend ASP.NET CORE 5

---
+++ backend/CONTATOCHAT_API/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using CONTATOCHAT_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CONTATOCHAT_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private static List<NovoContato> contatos = new List<NovoContato>();
+
+        [HttpPost]
+        public void ContatoCreate([FromBody] NovoContato contato)
+        {
+            contatos.Add(contato);
+        }
+
+
+        [HttpGet]
+        public IEnumerable<NovoContato> RetornarContatos()
+        {
+            return contatos;
+        }
+    }
+}
---
+++ backend/CONTATOCHAT_API/Models/Contato.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CONTATOCHAT_API.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+
+        public string nome {get; set;}
+
+        public string email { get; set;}
+
+        public string telefone { get; set;}
+
+        public string senha { get; set;}
+
+        public string foto_perfil { get; set;}
+
+    }
+
+
+    public class NovoContato
+    {
+        [Required(ErrorMessage = "É preciso informar o nome")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "É preciso informar o email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido.")]
+        public string email { get; set; }
+        
+        [Required(ErrorMessage = "É preciso informar o telefone")]
+        [RegularExpression("[0-9]+",ErrorMessage ="Informe somente os números")]
+        public string telefone { get; set; }
+
+        [Required(ErrorMessage = "É preciso informar uma senha")]
+        public string senha { get; set; }
+
+        public string foto_perfil { get; set; }
+
+    }
+}
