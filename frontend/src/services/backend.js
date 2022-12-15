422162e6006068befaf0066ba8be918ba3c7f46e
Daniel Caitano <danielcaitano9@hotmail.com>
2022-12-15 11:29:26 -0300

    ajustes

--- frontend/src/services/backend.js
+++ frontend/src/services/backend.js
@@ -1,22 +1,21 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:5000'
+const api = axios.create({
+    baseURL: 'http://localhost:60731',
+    headers:{
+        "Content-Type": "application/json; charset=utf-8"
+    },
+    transformRequest: [
+        (data, headers) => {
+            const token = localStorage.getItem('user')
+            if (token) headers.common.Authorization = `Bearer ${token}`
+            return data
+        }
+    ]
+})
 
-// const api = axios.create({
-//     baseUrl: baseUrl,
-//     transformRequest: [
-//         (data, headers) => {
-//             const token = localStorage.getItem('user')
-//             if (token) headers.common.Authorization = `Bearer ${token}`
-//             return data
-//         }
-//     ]
-// })
-
-
-
-const apiRoutes = {
-
-    registration:(user) => axios.post("http://localhost:5000/Account/Create", user)
+const apiRoute = {
+    registration: (user) => api.post("/Account/Create", user)   
 }
-export default apiRoutes
+
+export default apiRoute
