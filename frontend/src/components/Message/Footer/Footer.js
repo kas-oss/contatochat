be4cdd37258e226edb57fa497df4feb741fb242f
Daniel <danielcaitano9@hotmail.com>
2023-01-24 23:56:49 -0300

    ajustando

--- frontend/src/components/Message/Footer/Footer.js
+++ frontend/src/components/Message/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="chat-box-input">
             <input type="text" placeholder="Digite sua mensagem..." />
-            <input type="button" value={"enviar"} onClick={() => console.log(api.loadMessage())} />
+            <input type="button" value={"enviar"} onClick={async() => console.log(await api.loadMessage())} />
         </div>
     )
 }
